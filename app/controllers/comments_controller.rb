2d4b2572d1034ba8befcecf4d61c5c1adaadbaf8
Elise Brown <elisemarie88@gmail.com>
2021-08-26 19:13:24 -0600

    Add new comment works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,12 +10,34 @@ class CommentsController < ApplicationController
   end
 
   def new
+    render component: "NewComment", props:{item:@item}
   end
+
+  def create
+    @comment = @item.comments.new(comment_params)
+    if(@comment.save)
+      redirect_to item_comments_path(@item.id)
+    else
+    end
+  end
+
+  # def create
+  #   @item = @department.items.new(item_params)  # The boolean might be throwing things off
+  #   if(@item.save)
+  #     redirect_to department_items_path(@department.id)
+  #   else
+  #   end
+  # end
+
 
   def edit
   end
 
 private 
+
+  def comment_params
+    params.require(:comment).permit(:title, :body)
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
--- app/javascript/components/NewComment.js
+++ app/javascript/components/NewComment.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const NewComment = (props) => {  //this is the hash from the items controller#new 
-  const { department } = props;
+  const { item } = props;
   return (
     <div>
       <h1>New Comment</h1>
 
       <form action={`/items/${item.id}/comments`} method="post">
-        <p>Name</p>
-        <input />
-        <p>Sale true or false:</p>
-        <input />
+        <p>Title</p>
+        <input name="comment[title]"/>
+        <p>Comment</p>
+        <input name="comment[body]"/>
         <button>Add</button>
       </form>
     </div>
